cbaa2c26ef8d5e6d57dcaf3365beecc9cef0c76f
zDoki <165213924+zDoki@users.noreply.github.com>
2025-06-21 17:45:49 -0500

    Update1 app_script.js

--- app_script.js
+++ app_script.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.getElementById('sidebar');
+    const menuToggle = document.getElementById('menuToggle');
+    const pageSections = document.querySelectorAll('.page-section');
+    const navLinks = document.querySelectorAll('.main-nav a');
+    const pageTitle = document.getElementById('pageTitle');
+    const userNameDisplay = document.getElementById('userName');

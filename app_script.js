0964cd08d4c7d0bf0e92d55feef90f857e6b9011
zDoki <165213924+zDoki@users.noreply.github.com>
2025-06-21 17:46:07 -0500

    Update2 app_script.js

--- app_script.js
+++ app_script.js
@@ -5,3 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('.main-nav a');
     const pageTitle = document.getElementById('pageTitle');
     const userNameDisplay = document.getElementById('userName');
+
+    
+    const authScreen = document.getElementById('auth-screen');
+    const authForm = document.getElementById('authForm');
+    const authTitle = document.getElementById('authTitle');
+    const passwordField = document.getElementById('passwordField');
+    const nameUniversityCareerFields = document.getElementById('nameUniversityCareerFields');
+    const authSubmitBtn = document.getElementById('authSubmitBtn');
+    const toggleAuthLink = document.getElementById('toggleAuthLink');
+    const toggleAuthText = document.getElementById('toggleAuthText');
+    const authMessage = document.getElementById('authMessage');
